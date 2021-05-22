c428e82f1a5cc5067adf6e42be7c4d177529c5de
KayakerS6 <81826132+KayakerS6@users.noreply.github.com>
2021-05-22 12:38:42 -0700

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,14 +9,41 @@ six = str(6)
 seven = str(7)
 eight = str(8)
 nine = str(9)
+move = 0
 
-print(" " + one + " | " + two + " | " + three + " \n---|---|---\n " + four + " | " + five + " | " + six + " \n---|---|---\n " + seven + " | " + eight + " | " + nine + " ")
+positions = [1,2,3,4,5,6,7,8,9]
 
-move = int(input("Enter the number of the place you want the X to appear: "))
+def BoardStatus():
+	print(" " + one + " | " + two + " | " + three + " \n---|---|---\n " + four + " | " + five + " | " + six + " \n---|---|---\n " + seven + " | " + eight + " | " + nine + " ")
 
-if move == 1:
-	one = str("X")
-elif move == 2:
-	two = str("X")
+def PlayerAction():
+	move = int(input("Enter the number of the place you want the X to appear: "))
+	return(move)
+	
+def MoveMap(move):
+	if move == 1:
+		one = str("X")
+	elif move == 2:
+		two = str("X")
+	elif move == 3:
+		three = str("X")
+	elif move == 4:
+		four = str("X")
+	elif move == 5:
+		five = str("X")
+	elif move == 6:
+		six = str("X")
+	elif move == 7:
+		seven = str("X")
+	elif move == 8:
+		eight = str("X")
+	elif move == 9:
+		nine = str("X")
+	return("x")
 
-print(" " + one + " | " + two + " | " + three + " \n---|---|---\n " + four + " | " + five + " | " + six + " \n---|---|---\n " + seven + " | " + eight + " | " + nine + " ")
+BoardStatus()
+PlayerAction()
+MoveMap(move)
+
+BoardStatus()
+#print(" " + one + " | " + two + " | " + three + " \n---|---|---\n " + four + " | " + five + " | " + six + " \n---|---|---\n " + seven + " | " + eight + " | " + nine + " ")
